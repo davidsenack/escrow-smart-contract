561e622c69330442368bb58db22376c812358ef0
David Senack <david.senack@gmail.com>
2021-08-19 15:33:06 +0000

    added "ganache" dev network to truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -40,7 +40,12 @@ module.exports = {
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
-    //
+    // 
+    ganache: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",  
+    },
     // development: {
     //  host: "127.0.0.1",     // Localhost (default: none)
     //  port: 8545,            // Standard Ethereum port (default: none)
