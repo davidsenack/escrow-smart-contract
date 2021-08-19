48590c44281361977b6e082fc2681a9c988b376f
David Senack <david.senack@gmail.com>
2021-08-19 16:23:38 +0000

    changed network

--- migrations/2_escrow_migration.js
+++ migrations/2_escrow_migration.js
@@ -1,5 +1,5 @@
 const Escrow = artifacts.require('Escrow');
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deployer(Escrow, ganache, accounts[0])
+    deployer.deployer(Escrow, accounts[0])
 };
